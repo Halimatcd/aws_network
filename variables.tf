5464f9df719cf0aa375cf5db23d794b51de21e17
EC2 Default User <ec2-user@ip-172-31-90-28.ec2.internal>
2026-06-17 18:36:08 +0000

    Adding my aws_network module

--- variables.tf
+++ variables.tf
@@ -1,34 +1,8 @@
-# Default tags
-variable "default_tags" {
-  default = {}
-  type        = map(any)
-  description = "Default tags to be appliad to all AWS resources"
-}
-
-# Name prefix
-variable "prefix" {
-  type        = string
-  description = "Name prefix"
-}
-
-# Provision public subnets in custom VPC
-variable "private_cidr_blocks" {
-  default     = ["10.20.0.0/24", "10.20.1.0/24"]
-  type        = list(string)
-  description = "Public Subnet CIDRs"
-}
-
-# VPC CIDR range
-variable "vpc_cidr" {
-  default     = "10.20.0.0/16"
-  type        = string
-  description = "VPC to host static web site"
-}
-
-# Variable to signal the current environment 
-variable "env" {
-  default     = "dev"
-  type        = string
-  description = "Deployment Environment"
-}
-
+variable "aws_region"       { type = string }
+variable "name_prefix"      { type = string }
+variable "env"              { type = string }
+variable "instance_type"    { type = string }
+variable "key_name"         { type = string }
+variable "public_key_path"  { type = string }
+variable "subnet_id"        { type = string }
+variable "security_group_id"{ type = string }
