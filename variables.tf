e71c9a47bfea50382d891ee6897ac3328905edb7
EC2 Default User <ec2-user@ip-172-31-90-28.ec2.internal>
2026-06-18 02:14:24 +0000

    Replace webserver variables with network variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,19 @@
-variable "aws_region"       { type = string }
-variable "name_prefix"      { type = string }
-variable "env"              { type = string }
-variable "instance_type"    { type = string }
-variable "key_name"         { type = string }
-variable "public_key_path"  { type = string }
-variable "subnet_id"        { type = string }
-variable "security_group_id"{ type = string }
+variable "env" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "private_cidr_blocks" {
+  type = list(string)
+}
+
+variable "default_tags" {
+  type = map(string)
+}
